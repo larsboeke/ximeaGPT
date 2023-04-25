0d8178592e446ad407d6121779ec3496cae4dcfb
Christian <christian.zwiessler@gmail.com>
2023-04-25 11:23:30 +0200

    test2

---
+++ chunking_mails.py
@@ -0,0 +1,17 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+# This is a long document we can split up.
+with open('../../state_of_the_union.txt') as f:
+    state_of_the_union = f.read()
+
+
+text_splitter = RecursiveCharacterTextSplitter(
+    # Set a really small chunk size, just to show.
+    chunk_size = 100,
+    chunk_overlap  = 20,
+    length_function = len,
+)
+
+texts = text_splitter.create_documents([state_of_the_union])
+print(texts[0])
+print(texts[1])
