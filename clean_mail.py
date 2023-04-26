82d263ebc6c136c175518a82be854a9535394099
Christian <christian.zwiessler@gmail.com>
2023-04-26 14:29:07 +0200

    created method to clean Emails from emails-case-36517.csv and save it into solution.csv

--- clean_mail.py
+++ clean_mail.py
@@ -11,16 +11,42 @@ def extrahiere_text_aus_html(html_inhalt):
     reiner_text = soup.get_text()
     return reiner_text
 
-# Ersetzen Sie "beispiel.html" durch den Pfad zu Ihrer HTML-Datei
-html_dateipfad = "example_mail.html"
-html_inhalt = lade_html_datei_als_string(html_dateipfad)
-text = extrahiere_text_aus_html(html_inhalt)
+def remove_html(text):
+    text = extrahiere_text_aus_html(text)
+    return text
 
-def remove_from_to_subject(email_text):
+def remove_from_to_subject(text):
     pattern = r"From:.*?Subject:"
-    cleaned_email_text = re.sub(pattern, "", email_text, flags=re.DOTALL)
-    return cleaned_email_text.strip()
+    cleaned_text = re.sub(pattern, "", text, flags=re.DOTALL)
+    return cleaned_text.strip()
 
-text = remove_from_to_subject(text)
+def remove_text_between_keywords(text):
+    start_keywords = ["Best regards", "Sincerely", "Kind regards", "Regards", "Best wishes", "Yours faithfully",
+                      "Yours sincerely", "Warm regards", "All the best", "Cheers", "Take care",
+                      "Have a nice day", "With appreciation", "Cordially"]
+    end_keyword = "From:"
+    def replacement(match):
+        return match.group(1) + end_keyword
+    # Remove every signature except the last one
+    regex_pattern = f"({'|'.join(map(re.escape, start_keywords))}).*?{re.escape(end_keyword)}"
+    result = re.sub(regex_pattern, replacement, text, flags=re.DOTALL)
 
-print(text)
+    # Remove last signature
+    last_start_keyword_regex = f"({'|'.join(map(re.escape, start_keywords))})"
+    last_start_keyword_match = re.findall(last_start_keyword_regex, result)
+
+    if last_start_keyword_match:
+        last_start_keyword = last_start_keyword_match[-1]
+        index = result.rfind(last_start_keyword) + len(last_start_keyword)
+        result = result[:index]
+    return result
+
+def clean_text(text):
+    text = remove_html(text)
+    #print("Original ", text)
+    #print("Laenge: ", len(text))
+    text = remove_text_between_keywords(text)
+    text = remove_from_to_subject(text)
+    #print("\ngekürzter Text ",text)
+    #print("Laenge gekürzt: ", len(text))
+    return text
