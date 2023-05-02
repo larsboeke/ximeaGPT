cfe248728addc646056d2e5452b23296f511e3ad
Christian <christian.zwiessler@gmail.com>
2023-05-02 13:52:24 +0200

    explained output

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from prepare_email import prepare_email
 
-
-prepare_email(earliest_fetch_date="2010-01-01 00:00:00.000", index=0)
+# Output structure: text, date, communicationPartner, source
+email = prepare_email(earliest_fetch_date="2010-01-01 00:00:00.000", index=0)
