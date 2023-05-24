bd69cf5cf0dc726749f704bde128b513232931df
Mahdi Ghanem <mghanem.wi@gmail.com>
2023-05-24 12:29:08 +0000

    Upload New File

---
+++ TicketsFiles/Chunk.py
@@ -0,0 +1,17 @@
+class Chunk:
+
+    def __init__(self, Text, Ticket, Order):
+        self.Text = Text
+        self.Ticket = Ticket
+        self.Order = Order
+
+    def get_Text(self):
+        return self.Text
+
+    def get_Ticket(self):
+        return self.Ticket
+
+    def get_Order(self):
+        return self.Order
+
+
