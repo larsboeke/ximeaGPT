f89bc63d2650d9ebbae65bdfc5c062349ec0e56f
lorenzbokemeyer <lbokemeyer@googlemail.com>
2023-05-24 14:03:51 +0200

    selber status

--- initialPush.py
+++ initialPush.py
@@ -5,19 +5,19 @@ from process_emails.get_cases_from_db import get_all_cases
 # Upload PDFs
 for pdf in manuals.pdf_list:
     uploadData.uploadPDF(pdf)
-    print(pdf + "uploaded")
+   
 
 # Upload URLs
 for url in manuals.url_list:
     uploadData.uploadURL(url)
-    print(url + "uploaded")
+
 
 # Upload Emails
 i = 0
 anzahl_cases = len(get_all_cases())
 for case in get_all_cases():
     i += 1
-    print(i + "/" + anzahl_cases + "uploaded")
+
     uploadData.uploadMail(case)
     if i == 40:
         break
