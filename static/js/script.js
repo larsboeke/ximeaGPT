dd218eaf7f7a7ac7157118c1cf96a250217527db
Maria Zhaak <mzhaak@uni-muenster.de>
2023-06-15 14:37:16 +0200

    Updated a history sidebar, added time

--- static/js/script.js
+++ static/js/script.js
@@ -2,10 +2,14 @@ const chatInput = document.querySelector("#chat-input");
 const sendButton = document.querySelector("#send-btn");
 const chatContainer = document.querySelector(".chat-container");
 const themeButton = document.querySelector("#theme-btn");
-const deleteButton = document.querySelector("#delete-btn")
-const uploadButton = document.querySelector("#upload-btn")
-const fileInfo = document.querySelector(".file-info")
+const deleteButton = document.querySelector("#delete-btn");
+const uploadButton = document.querySelector("#upload-btn");
+const fileInfo = document.querySelector(".file-info");
+const newChatButton = document.querySelector("#new-chat-btn");
+const history = document.querySelector(".history");
+const chatNum = 0;
 const socket = io.connect('http://localhost:5000');
+
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
@@ -34,7 +38,7 @@ const loadDataFromLocalstorage = () => {
                             <p>Start a conversation and explore the power of AI.<br> Your chat history will be displayed here.</p>
                         </div>`
 
-    chatContainer.innerHTML = localStorage.getItem("chat-history") || defaultText;
+    chatContainer.innerHTML = localStorage.getItem(`chat-history ${chatNum}`) || defaultText;
     //automatic scrolldown
     chatContainer.scrollTo(0, chatContainer.scrollHeight);
 }
@@ -52,9 +56,13 @@ const createChatElement = (html, className) => {
 const getChatResponse = (aiChatDiv) =>{
     const pElement = document.createElement("p");
     aiChatDiv.querySelector(".chat-details").appendChild(pElement);
+    const timeElement = document.createElement("span");
+    timeElement.className = "time";
+    aiChatDiv.querySelector(".chat-details").appendChild(timeElement);
 
     const receiveResponse = (msg) => {
         pElement.textContent = msg.trim();
+        timeElement.textContent = getCurrentTime();
         aiChatDiv.querySelector(".typing-animation").remove();
         socket.off('backend_message', receiveResponse);
     };
@@ -67,25 +75,8 @@ const getChatResponse = (aiChatDiv) =>{
         aiChatDiv.querySelector(".typing-animation").remove();
         socket.off('backend_message', receiveResponse);
     });
-    // try {
-    //     socket.on('backend_message', (msg) => {
-    //         pElement.textContent = msg.trim();
-    //         aiChatDiv.querySelector(".typing-animation").remove();
-    //         aiChatDiv.querySelector(".chat-details").appendChild(pElement);
-    //     })
-    // } catch(error){
-    //     socket.on('backend_error',(error)=>
-    //     {
-    //         pElement.classList.add("error");
-    //         pElement.textContent = "Oops! Something went wrong while retrieving the response. Please try again.";
-    //         aiChatDiv.querySelector(".typing-animation").remove();
-    //         socket.off('backend_message');
-    //         });
-    //     // pElement.classList.add("error");
-    //     // pElement.textContent ="Ooops! Something went wtrong while retrievig the response.Please try again.";
-    // }
     //saving all chat HTML data as chat-hystory name in local storage
-    localStorage.setItem("chat-history", chatContainer.innerHTML)
+    localStorage.setItem(`chat-history ${chatNum}`, chatContainer.innerHTML)
     //automatic scrolldown
     chatContainer.scrollTo(0, chatContainer.scrollHeight);
 }
@@ -101,7 +92,7 @@ const copyResponse = (copyBtn) => {
 const showTypingAnimation = () => {
     const html =`<div class="chat-content">
                     <div class="chat-details">
-                        <img src="../static/images/AI_Lean.png" alt="chatbot-img">
+                        <img src="../static/images/gpt_logo.png" alt="chatbot-img">
                         <div class="typing-animation">
                             <div class="typing-dot" style="--delay: 0.2s"></div>
                             <div class="typing-dot" style="--delay: 0.3s"></div>
@@ -112,7 +103,7 @@ const showTypingAnimation = () => {
                     <span onclick="copyResponse(this)" class="material-symbols-rounded">content_copy</span>
                     <!--TO-DO:Feedback buttons onclick functionality-->
                     <span id="thumb-up" class="material-symbols-outlined">thumb_up</span>
-                    <span id="thumb-down" class="material-symbols-outlined">thumb_down</span>
+                    <span onclick="negFeedback" id="thumb-down" class="material-symbols-outlined">thumb_down</span>
                     </div>
                 </div>`;
     const aiChatDiv = createChatElement(html, "backend");
@@ -122,47 +113,30 @@ const showTypingAnimation = () => {
     getChatResponse(aiChatDiv);
 }
 
+const getCurrentTime = () =>{
+    let dateObject = new Date();
+    let cDate = dateObject.getDate() + '/' + (dateObject.getMonth() + 1) + '/' + dateObject.getFullYear();
+    let cTime = dateObject.getHours() + ":" + dateObject.getMinutes() + ":" + dateObject.getSeconds();
+    return cDate + ' ' + cTime;
+}
+
 const handleUserMessage = () => {
     if(chatInput.value){
         socket.emit('client_message', chatInput.value);
         const html =`<div class="chat-content">
-                    <div class="chat-details">
-                        <img src="../static/images/user_logo.png" alt="user-img">
-                        <p>${chatInput.value}</p>
-                    </div>
-                </div>`;
+                        <div class="chat-details">
+                            <img src="../static/images/user_logo.png" alt="user-img">
+                            <p>${chatInput.value}</p>
+                            <span class="time">${getCurrentTime()}</span>
+                        </div>
+                    </div>`;
         const userChatDiv = createChatElement(html, "client");
         document.querySelector(".default-text")?.remove();
         chatContainer.appendChild(userChatDiv);
         chatInput.value = " " //clear the textarea after sending
         chatInput.style.height = `${initialHeight}px`;
     } 
-
-    socket.on('backend_message', (msg) => {
-        const pElement = document.createElement("p");
-        pElement.textContent = msg.trim();
-        const html =`<div class="chat-content">
-                    <div class="chat-details">
-                        <img src="../static/images/AI_Lean.png" alt="chatbot-img">
-                        <div class="typing-animation">
-                            <div class="typing-dot" style="--delay: 0.2s"></div>
-                            <div class="typing-dot" style="--delay: 0.3s"></div>
-                            <div class="typing-dot" style="--delay: 0.4s"></div>
-                         </div>
-                    </div>
-                    <div class="chat-controls">
-                    <span onclick="copyResponse(this)" class="material-symbols-rounded">content_copy</span>
-                    <!--TO-DO:Feedback buttons onclick functionality-->
-                    <span onclick="posFeedback()" id="thumb-up" class="material-symbols-outlined">thumb_up</span>
-                    <span onclick="negFeedback()" id="thumb-down" class="material-symbols-outlined">thumb_down</span>
-                    </div>
-                </div>`;
-        const aiChatDiv = createElement(html, "backend");
-        chatContainer.appendChild(aiChatDiv);
-        aiChatDiv.querySelector(".typing-animation").remove();
-        aiChatDiv.querySelector(".chat-details").appendChild(pElement);
-        chatContainer.scrollTo(0, chatContainer.scrollHeight);
-    });  
+    showTypingAnimation();
     chatContainer.scrollTo(0, chatContainer.scrollHeight);
 }
 
@@ -175,7 +149,7 @@ themeButton.addEventListener("click", () =>{
 deleteButton.addEventListener("click", () =>{
     //TO-DO: change confirm window to a centered pop-up window
     if(confirm("Are you sure that you want to delete the history of this chat?")){
-        localStorage.removeItem("chat-history");
+        localStorage.removeItem(`chat-history ${chatNum}`);
         fileInfo.remove();
         loadDataFromLocalstorage();
     }
@@ -240,6 +214,17 @@ chatInput.addEventListener("keydown", (e) => {
 })
 
 sendButton.addEventListener("click", handleUserMessage);
+
+newChatButton.addEventListener("click", () => {
+    // chatDiv = localStorage.getItem(`chat-history ${chatNum}`);
+    // const pElement = document.createElement("p");
+    // pElement.textContent = `chat ${chatNum}`
+    // history.appendChild(pElement);
+    // chatContainer.remove();
+    // chatNum++;  
+    // console.log(chatNum);
+});
+
 
 // Sidebar Close and Open Button
     /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
