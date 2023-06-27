78823baf4d428f57e8d0e4aec67e147fc1a82726
lennart317 <lennart.dammer@gmail.com>
2023-06-27 18:35:12 +0200

    not working: email uploader

--- data_package/MongoDB_Connection_Provider/MongoDBConnectionProvider.py
+++ data_package/MongoDB_Connection_Provider/MongoDBConnectionProvider.py
@@ -1,13 +1,14 @@
 import pymongo
+from pymongo import MongoClient, UuidRepresentation
 
 class MongoDBConnectionProvider:
     def initMongoDB(self):
         """
         Initialise a connection to our MongoDB database
-        :return col: collection name of the database to connect to
+        :return mongodb_connection: collection name of the database to connect to
         """
         # Initialise the non structured database MongoDB
-        client = pymongo.MongoClient("mongodb://192.168.11.30:27017/")
+        client = pymongo.MongoClient("mongodb://192.168.11.30:27017/", uuidRepresentation= UuidRepresentation.STANDARD)
         db = client["XIMEAGPT"]
-        col = db["prototype"]
-        return col
+        mongodb_connection = db["prototype2"]
+        return mongodb_connection
--- initialPush_email.py
+++ initialPush_email.py
@@ -1,14 +1,3 @@
-from .URL_Handler import manuals
-import uploadData
-from process_emails_old.get_cases_from_db import get_all_cases
-from process_tickets.resolved_ids import ticket_ids
+from Uploader import Uploader
 
-# Approx 42€ total costs
-
-# Upload Emails
-# Approx costs 0,0004 * 0,4 * 8 * 29 750
-iter=0
-for case in get_all_cases():
-    uploadData.uploadMail(case)
-    iter += 1
-    print("Uploaded case ", iter, " / ", len(get_all_cases()))
+Uploader().uploadMails()
