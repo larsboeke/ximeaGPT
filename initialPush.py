73eee63b98b48243bf7545cf52aaf5210321af61
lennart317 <lennart.dammer@gmail.com>
2023-06-29 16:48:01 +0200

    runnable initialpush

--- initialPush.py
+++ initialPush.py
@@ -7,20 +7,30 @@ from data_package import resolved_ids
 
 Uploader().initialUploadMail()
 
+print("All Emails uploaded!")
+
 iter=0
 for path in manual_url_list.pdf_list:
     Uploader().uploadPDF(path)
     iter += 1
     print("Uploaded case ", iter, " / ", len(manual_url_list.pdf_list))
 
-"""iter=0
+print("All Manuals uploaded!")
+
+
+iter=0
 for url in manual_url_list.url_list:
     Uploader().uploadURL(url)
     iter += 1
-    print("Uploaded case ", iter, " / ", len(manual_url_list.url_list))"""
+    print("Uploaded case ", iter, " / ", len(manual_url_list.url_list))
 
+print("All URLs uploaded!")
 
-"""for id in resolved_ids.ticket_ids:
+          
+for id in resolved_ids.ticket_ids:
     Uploader().uploadTicket(id)
     iter += 1
-    print("Uploaded Ticket ", iter, " / ", len(resolved_ids.ticket_ids))"""
+    print("Uploaded Ticket ", iter, " / ", len(resolved_ids.ticket_ids))
+
+print("All Tickets uploaded!")
+
