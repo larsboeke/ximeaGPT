04899f3dedb360e320043d32da90260c5047e841
Lars Boeke <Lars.Boeke@ximea-group.com>
2023-07-13 23:30:23 +0200

    Hardcode SQL-Agent

--- agent/AIResponse.py
+++ agent/AIResponse.py
@@ -134,16 +134,18 @@ class AiResponse:
             elif function_name == "get_last_message":
                 pass
 
-            elif function_name == "get_database_schema":
-                print("Using get_database_schema tool...")
-                function_response = Agent_functions.get_database_schema()
-
-            elif function_name == "query_product_database":
-                print("Using query_product_database tool...")
-                function_response = Agent_functions.query_product_database( # Eventually add sources!
-                    sqlquery = data["sqlquery"]
+            elif function_name == "query_feature_of_product_pdb":
+                print("Using query_feature_of_product_pdb tool...")
+                function_response = Agent_functions.query_feature_of_product_pdb( # Eventually add sources!
+                    product = data["product"]
                 )
-                #self.sources.append(source)
+                print(function_response)
+            elif function_name == "query_data_of_feature_of_product_pdb":
+                print("Using query_data_of_feature_of_product_pdb tool...")
+                function_response = Agent_functions.query_data_of_feature_of_product_pdb( # Eventually add sources!
+                    product=data["product"], feature=data["feature"], category=data["category"]
+                )
+                print(function_response)
 
 
             print(check_function_call)
