906f8a47a7f55d9f1b98906a64ce58c8078ada15
lorenzbokemeyer <lbokemeyer@googlemail.com>
2023-07-19 14:41:59 +0200

    try except added in airesponse

--- agent/AIResponse.py
+++ agent/AIResponse.py
@@ -71,96 +71,101 @@ class AiResponse:
 
     def chat_completion_request(self):
         
-        self.add_user_message(self.user_prompt)
+        try:
         
-        message = self.get_openai_response("auto")
+            self.add_user_message(self.user_prompt)
+            
+            message = self.get_openai_response("auto")
+        
+            check_function_call = message.get("function_call")
+            message['timestamp'] = str(dt.now())
+
+            assistant_message = message['content']
+            if not check_function_call:
+                self.add_assistant_message(message['content'], [])
+
+            function_call_counter = 0
+            function_call_limit = 1
+            query_counter = 1
+
+            while function_call_counter < function_call_limit and check_function_call:  
     
-        check_function_call = message.get("function_call")
-        message['timestamp'] = str(dt.now())
+                json_str = message["function_call"]["arguments"]
+                data = json.loads(json_str)
+                function_name = message["function_call"]["name"]
 
-        assistant_message = message['content']
-        if not check_function_call:
-            self.add_assistant_message(message['content'], [])
+                if function_name == "query_unstructured_data":
+                    print("Using query_unstructured_data tool...")
+                    function_response, sources, tokens = Agent_functions.getText(
+                        query=data["query"],
+                        counter = query_counter
+                    )
+                    # append sources to sources attribute
+                    for source in sources:
+                        self.sources.append(source)
+                    # app used tokens
+                    self.embeddings_tokens += tokens
+                    query_counter += 1
+                    print(function_response)
 
-        function_call_counter = 0
-        function_call_limit = 1
-        query_counter = 1
+                elif function_name == "query_feature_of_product_pdb":
+                    print("Using query_feature_of_product_pdb tool...")
+                    function_response, sources = Agent_functions.query_feature_of_product_pdb( # Eventually add sources!
+                        product = data["product"]
+                    )
+                    for source in sources:
+                        self.sources.append(source)
+                    print(function_response)
 
-        while function_call_counter < function_call_limit and check_function_call:  
- 
-            json_str = message["function_call"]["arguments"]
-            data = json.loads(json_str)
-            function_name = message["function_call"]["name"]
+                # elif function_name == "query_data_of_category_feature_of_product_pdb":
+                #     print("Using query_data_of_category_feature_of_product_pdb tool...")
+                #     function_response = Agent_functions.query_data_of_category_feature_of_product_pdb( # Eventually add sources!
+                #         product=data["product"], feature=data["feature"], category=data["category"]
+                #     )
+                #     print(function_response)
 
-            if function_name == "query_unstructured_data":
-                print("Using query_unstructured_data tool...")
-                function_response, sources, tokens = Agent_functions.getText(
-                    query=data["query"],
-                    counter = query_counter
-                )
-                # append sources to sources attribute
-                for source in sources:
-                    self.sources.append(source)
-                # app used tokens
-                self.embeddings_tokens += tokens
-                query_counter += 1
-                print(function_response)
+                elif function_name == "query_data_of_feature_of_product_pdb":
+                    print("Using query_data_of_feature_of_product_pdb tool...")
+                    function_response = Agent_functions.query_data_of_feature_of_product_pdb( # Eventually add sources!
+                        product=data["product"], feature=data["feature"]
+                    )
+                    print(function_response)
+                    for source in sources:
+                        self.sources.append(source)
 
-            elif function_name == "query_feature_of_product_pdb":
-                print("Using query_feature_of_product_pdb tool...")
-                function_response, sources = Agent_functions.query_feature_of_product_pdb( # Eventually add sources!
-                    product = data["product"]
-                )
-                for source in sources:
-                    self.sources.append(source)
-                print(function_response)
+                print(check_function_call)
+                self.add_function(function_name, str(function_response))
 
-            # elif function_name == "query_data_of_category_feature_of_product_pdb":
-            #     print("Using query_data_of_category_feature_of_product_pdb tool...")
-            #     function_response = Agent_functions.query_data_of_category_feature_of_product_pdb( # Eventually add sources!
-            #         product=data["product"], feature=data["feature"], category=data["category"]
-            #     )
-            #     print(function_response)
+                function_call_counter += 1
 
-            elif function_name == "query_data_of_feature_of_product_pdb":
-                print("Using query_data_of_feature_of_product_pdb tool...")
-                function_response = Agent_functions.query_data_of_feature_of_product_pdb( # Eventually add sources!
-                    product=data["product"], feature=data["feature"]
-                )
-                print(function_response)
-                for source in sources:
-                    self.sources.append(source)
+                #call response with functions if counter is lower than function call limit, otherwise force respose without functions
+                if function_call_counter == function_call_limit:
+                    message_response_to_function = self.get_openai_response(call_type="none")
+                
+                elif function_call_counter < function_call_limit:
+                    message_response_to_function = self.get_openai_response(call_type="auto")
+        
+                
+                assistant_message = message_response_to_function['content']
+                self.add_assistant_message(assistant_message, self.sources)
+                
+                print(f"Conversation History ------------------------------------------------ \n {self.conversation_history}")
+                print(f"prompt_tokens {self.prompt_tokens} , completion_tokens {self.completion_tokens} , embeddings_tokens {self.embeddings_tokens}")
 
-            print(check_function_call)
-            self.add_function(function_name, str(function_response))
+                conv_his_token = num_tokens_from_string(str(self.conversation_history[1]), "cl100k_base")
+                print(f"Token of History {conv_his_token}")
 
-            function_call_counter += 1
+                # if conv_his_token > 4000:
+                #    self.prompt_tokens = self.prompt_tokens - num_tokens_from_string(self.conversation_history[1], "cl100k_base")
+                #    self.conversation_history.pop(1)
+                #    print("Dropped one")
 
-            #call response with functions if counter is lower than function call limit, otherwise force respose without functions
-            if function_call_counter == function_call_limit:
-                message_response_to_function = self.get_openai_response(call_type="none")
-            
-            elif function_call_counter < function_call_limit:
-                message_response_to_function = self.get_openai_response(call_type="auto")
-    
-            
-            assistant_message = message_response_to_function['content']
-            self.add_assistant_message(assistant_message, self.sources)
-            
-            print(f"Conversation History ------------------------------------------------ \n {self.conversation_history}")
-            print(f"prompt_tokens {self.prompt_tokens} , completion_tokens {self.completion_tokens} , embeddings_tokens {self.embeddings_tokens}")
+            act.add_activity(self.embeddings_tokens, self.prompt_tokens, self.completion_tokens, self.start_timestamp, end_timestamp = dt.now())
 
-            conv_his_token = num_tokens_from_string(str(self.conversation_history[1]), "cl100k_base")
-            print(f"Token of History {conv_his_token}")
-
-            # if conv_his_token > 4000:
-            #    self.prompt_tokens = self.prompt_tokens - num_tokens_from_string(self.conversation_history[1], "cl100k_base")
-            #    self.conversation_history.pop(1)
-            #    print("Dropped one")
-
-        act.add_activity(self.embeddings_tokens, self.prompt_tokens, self.completion_tokens, self.start_timestamp, end_timestamp = dt.now())
-
-        return assistant_message, self.sources
+            return assistant_message, self.sources
+        except Exception as e:
+            print(e)
+            return "An Error occured, try again", self.sources
 
 def num_tokens_from_string(string: str, encoding_name: str) -> int:
     """Returns the number of tokens in a text string."""
