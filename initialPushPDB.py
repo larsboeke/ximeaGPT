405213dc881911fc30778eedfe2ad035cc7fe32e
Christian Zwiessler <christian.zwiessler@gmail.com>
2023-07-28 13:08:51 +0000

    Update initialPushPDB.py

--- initialPushPDB.py
+++ initialPushPDB.py
@@ -4,7 +4,6 @@ import data_package.SQL_Connection_Provider.SQLConnectionProvider as SQLConnecti
 
 sql_connection = SQLConnectionProvider.SQLConnectionProvider().create_connection()
 
-# init PDB push
 UpdateUploader().fetchingStagingPDB()
 PDBSetup(sql_connection).settingUpPDB()
 PDBSetup(sql_connection).pushingNewFeaturesToPinecone()
